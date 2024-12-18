39839b4265f7db1959cd93edc6f4a5a129af4147
thinkphp <thinkphp@qq.com>
2024-12-18 17:38:38 +0800

    调整type

--- src/model/type/Date.php
+++ src/model/type/Date.php
@@ -4,11 +4,13 @@ declare (strict_types = 1);
 
 namespace think\model\type;
 
+use think\model\contract\Modelable;
+
 class Date extends DateTime
 {
     protected $data;
 
-    public static function from(mixed $value)
+    public static function from(mixed $value, Modelable $model)
     {
         $static = new static();
         $static->data($value, 'Y-m-d');
